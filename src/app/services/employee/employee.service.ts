395f8284b96a589002659e7832aff207f965f4fc
kayyborra <u19020393@tuks.co.za>
2021-08-20 20:48:22 +0200

    added employee service methods

--- src/app/services/employee/employee.service.ts
+++ src/app/services/employee/employee.service.ts
@@ -1,9 +1,58 @@
 import { Injectable } from '@angular/core';
+import { DataService } from '../data/data.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  constructor() { }
+ 
+  constructor(
+    private dataService:DataService
+  ) { }
+
+  getAllEmployees() {
+    debugger
+    return this.dataService.genericCaller("get", "Employee/All", "");
+  }
+
+  addEmployee(employeeDate: any) {
+    debugger
+    return this.dataService.genericCaller("post", "Employee/Upsert", employeeDate);
+  }
+
+  getEmployeeById(employeeId: number) {
+    debugger
+    return this.dataService.genericCaller("get", `Employee/${employeeId}`, "");
+  }
+
+  getEmployeeByUserId(userId: number) {
+    debugger
+    return this.dataService.genericCaller("get", `Employee/User/${userId}`, "");
+  }
+
+  deleteEmployee(employeeUserId: number) {
+    debugger
+    return this.dataService.genericCaller("get", `Employee/Delete/${employeeUserId}`, "");
+  }
+
+  searchEmployeeByName(searchTerm:string)
+  {
+    return this.dataService.genericCaller("get","Employee/Search?name="+searchTerm,"");
+  }
+
+  employeeAttendanceHistory(dateInfo:string)
+  {
+    return this.dataService.genericCaller("get","Employee/EmployeeMonthlyAttendanceHistory?dateInfo="+dateInfo,"");
+  }
+
+  sixMonthRegistrationHistory()
+  {
+    return this.dataService.genericCaller("get","Employee/EmployeeSixMonthRegistrationHistory","");
+  }
+
+  updateEmployee(employeeDate: any) {
+    debugger
+    return this.dataService.genericCaller("post", "Employee/Update", employeeDate);
+  }
 }
