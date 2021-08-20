76efffba95f1d6581cab0747c20dc794298530ac
kayyborra <u19020393@tuks.co.za>
2021-08-20 21:00:43 +0200

    added team service methods

--- src/app/services/team/team.service.ts
+++ src/app/services/team/team.service.ts
@@ -1,9 +1,76 @@
 import { Injectable } from '@angular/core';
+import { DataService } from '../data/data.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TeamService {
 
-  constructor() { }
+  constructor(
+    private dataService:DataService
+  ) { }
+
+  getAllTeams() {
+    debugger
+    return this.dataService.genericCaller("get", "Team/All", "");
+  }
+
+  addTeam(teamData: any) {
+    debugger
+    return this.dataService.genericCaller("post", "Team/Add", teamData);
+  }
+
+  getTeamById(teamId: number) {
+    debugger
+    return this.dataService.genericCaller("get", `Team/${teamId}`, "");
+  }
+
+  deleteTeam(teamId: number) {
+    debugger
+    return this.dataService.genericCaller("get", `Team/Delete/${teamId}`, "");
+  }
+
+  searchTeamByName(searchTerm:string)
+  {
+    return this.dataService.genericCaller("get","Team/Search?team="+searchTerm,"");
+  }
+
+  getTeamEmployee(teamId:number)
+  {
+    return this.dataService.genericCaller("get","Team/Employee/"+teamId,"");
+  }
+
+  addEmployeeToTeam(employeeData:any)
+  {
+    return this.dataService.genericCaller("post","Team/Employee/Add",employeeData);
+  }
+  RemoveEmployeeFromTeam(teamId:number,employeeId:number)
+  {
+    return this.dataService.genericCaller("get","Team/Employee/Remove?teamId="+teamId+"&employeeId="+employeeId,"");
+  }
+
+  getWeekDays()
+  {
+    return this.dataService.genericCaller("get","Team/WeekDays","");
+  }
+  
+
+  getTeamAtendanceDetail(teamId:number)
+  {
+    return this.dataService.genericCaller("get","Team/AttendanceData/"+teamId,"");
+  }
+
+  getTeamAtendanceDetailForReport(teamId:number,date:string)
+  {
+    return this.dataService.genericCaller("get","Team/AttendanceReportData?teamId="+teamId+"&date="+date,"");
+
+  }
+
+
+  markTeamAttendance(attendanceDetail:any)
+  {
+    return this.dataService.genericCaller("post","Team/MarkAttendance",attendanceDetail);
+  }
+
+
 }
