b92b35c5a4368e518259d06781b3215484779c97
Ammaar1803 <u18080945@tuks.co.za>
2021-08-20 23:15:10 +0200

    added user service methods

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { DataService } from '../data/data.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
+
+  getUserTypes() {
+    return this.dataService.genericCaller("get", "User/UserTypes/All", "");
+  }
+
+  getAudit() {
+    return this.dataService.genericCaller("get", "User/Audit", "");
+  }
 }
