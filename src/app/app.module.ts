6fbd48304a84618f1056ed2d480000d4c6b894aa
JayyyHarden <u19107430@tuks.co.za>
2021-08-21 19:36:42 +0200

    Added Audit Component folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuditComponent } from './Admin/Audit/audit/audit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuditComponent
   ],
   imports: [
     BrowserModule,
