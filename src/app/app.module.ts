2f557e3f999fb1e81078cb903ecc0ccd00906d2f
ArisjaB <u19002328@tuks.co.za>
2021-08-21 19:49:44 +0200

    created order components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuditComponent } from './Admin/Audit/audit/audit.component';
+import { OrderComponent } from './admin/Order/order/order.component';
+import { ClientOrderDetailComponent } from './admin/Order/client-order-detail/client-order-detail.component';
+import { ViewClientOrderDetailComponent } from './admin/Order/view-client-order-detail/view-client-order-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuditComponent
+    AuditComponent,
+    OrderComponent,
+    ClientOrderDetailComponent,
+    ViewClientOrderDetailComponent
   ],
   imports: [
     BrowserModule,
