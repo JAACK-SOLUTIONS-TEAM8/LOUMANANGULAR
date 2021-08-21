f92134b010427656d5bfb88d089ca90cf6d9259f
ArisjaB <u19002328@tuks.co.za>
2021-08-21 20:38:14 +0200

    added getorders and the order details

--- src/app/Admin/Order/client-order-detail/client-order-detail.component.ts
+++ src/app/Admin/Order/client-order-detail/client-order-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { OrderService } from 'src/app/services/order/order.service';
 
 @Component({
   selector: 'app-client-order-detail',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client-order-detail.component.css']
 })
 export class ClientOrderDetailComponent implements OnInit {
-
-  constructor() { }
+  orderData:any[]=[]
+  constructor(
+    private orderService:OrderService,
+    private router:Router
+  ) { }
 
   ngOnInit(): void {
+    this.getAllOrders()
+  }
+  getAllOrders()
+  {
+    this.orderService.getAllOrders().subscribe(response=>{
+      this.orderData=response.orders
+    });
+  }
+
+
+  getOrderDetail(order:any)
+  {
+    this.router.navigateByUrl(`/admin/orders/detail/${order.orderId}`)
   }
 
 }
+
+
