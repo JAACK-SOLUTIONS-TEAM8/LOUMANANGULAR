9f0178db263fe557beb90fb41963e15cfa436c2b
ArisjaB <u19002328@tuks.co.za>
2021-08-21 16:20:21 +0200

    added order service code

--- src/app/services/order/order.service.ts
+++ src/app/services/order/order.service.ts
@@ -1,9 +1,68 @@
 import { Injectable } from '@angular/core';
+import { DataService } from '../data/data.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
 
-  constructor() { }
+  constructor(
+    private dataService:DataService
+  ) { }
+
+  getAllDeliveryTypes() {
+    debugger
+    return this.dataService.genericCaller("get", "Order/DeliveryType/All", "");
+  }
+
+  addDeliveryType(deliveryData: any) {
+    debugger
+    return this.dataService.genericCaller("post", "Order/DeliveryType/Add", deliveryData);
+  }
+
+  getDeliveryTypeById(deliveryTypeId: number) {
+    debugger
+    return this.dataService.genericCaller("get", `Order/DeliveryType/${deliveryTypeId}`, "");
+  }
+
+  deleteDeliveryType(deliveryTypeId: number) {
+    debugger
+    return this.dataService.genericCaller("get", `Order/DeliveryType/Delete/${deliveryTypeId}`, "");
+  }
+
+  searchDeliveryTypeByName(searchTerm:string)
+  {
+    return this.dataService.genericCaller("get","Order/DeliveryType/Search?name="+searchTerm,"");
+  }
+
+
+  submitOrder(orderDetail: any) {
+    debugger
+    return this.dataService.genericCaller("post", "Order/Add", orderDetail);
+  }
+
+  getAllOrders() {
+    debugger
+    return this.dataService.genericCaller("get", "Order/All", "");
+  }
+
+  getClientOrders(orderId:number) {
+    debugger
+    return this.dataService.genericCaller("get", `Order/All/${orderId}`, "");
+  }
+
+  completeOrder(orderId:number)
+  {
+    debugger
+    return this.dataService.genericCaller("get", `Order/Complete/${orderId}`, "");
+  }
+
+
+  getMonthlySalesReport(dateInfo:string)
+  {
+    debugger
+    return this.dataService.genericCaller("get", `Order/MonthlySalesReport?dateInfo=${dateInfo}`, "");
+  }
+
 }
+
