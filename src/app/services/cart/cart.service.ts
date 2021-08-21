fa862f9bfffe82243a4d1f25a4c4f130bd25dcdb
ArisjaB <u19002328@tuks.co.za>
2021-08-21 16:17:02 +0200

    added cart.service.ts code

--- src/app/services/cart/cart.service.ts
+++ src/app/services/cart/cart.service.ts
@@ -1,3 +1,4 @@
+import { stringify } from '@angular/compiler/src/util';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -6,4 +7,45 @@ import { Injectable } from '@angular/core';
 export class CartService {
 
   constructor() { }
+
+  addPrductToShoppingCart(products:any)
+  {
+    window.sessionStorage.setItem("products",JSON.stringify(products));
+    window.sessionStorage.setItem("itemCount",products.length.toString());
+  }
+
+  getProductsFromShoppingCart():any[]
+  {
+    var products=window.sessionStorage.getItem("products");
+    if(products!=null)
+    {
+      return JSON.parse(products);
+    }
+    else
+    {
+      return [];
+    }
+  }
+
+  getItemCountInCart()
+  {
+    return Number( window.sessionStorage.getItem("itemCount"));
+  }
+
+  clearCart()
+  {
+    window.sessionStorage.removeItem("products");
+    window.sessionStorage.setItem("itemCount","0");
+    return Number( window.sessionStorage.getItem("itemCount"));
+  }
+
+  removeItemFromCart(product:any)
+  {
+    var productsInCart=this.getProductsFromShoppingCart();
+    if(productsInCart.length!=0)
+    {
+      productsInCart.splice(productsInCart.indexOf(product),1);
+      this.addPrductToShoppingCart(productsInCart);
+    }
+  }
 }
