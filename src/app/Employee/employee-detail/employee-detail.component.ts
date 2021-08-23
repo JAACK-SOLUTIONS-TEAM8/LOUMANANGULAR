aeb63b4c62f61dc3e41d10a009e6de104d3fb3c0
kayyborra <u19020393@tuks.co.za>
2021-08-23 19:44:50 +0200

    added emp detail comp methods

--- src/app/Employee/employee-detail/employee-detail.component.ts
+++ src/app/Employee/employee-detail/employee-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { EmployeeService } from 'src/app/services/employee/employee.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-employee-detail',
@@ -7,9 +10,96 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeDetailComponent implements OnInit {
 
-  constructor() { }
+  selectedEmployee: any;
+  searchTerm!: string;
+
+  constructor(
+    private router: Router, 
+    private route: ActivatedRoute,
+    private employeeService:EmployeeService) { }
+    
+    employeesDetail:any[]=[]
 
   ngOnInit(): void {
+    this.getAllEmployees();
+  }
+  getAllEmployees()
+  {
+    this.employeeService.getAllEmployees().subscribe(response=>{
+
+      this.employeesDetail=response.employees;
+    });
+  }
+
+  editEmployee(employee:any) {
+    this.router.navigateByUrl(`/employee/add/${employee.employeeId}`);
+  }
+
+  confirmDeleteion() {
+    console.log(this.selectedEmployee);
+    this.employeeService.deleteEmployee(this.selectedEmployee.userId).subscribe(response => {
+      if(response.statusCode==200)
+      {
+        console.log("employee deleted successfully");
+        this.getAllEmployees();
+      }
+      console.log(response);
+    });
+  }
+
+  deleteEmployee(employee: any)
+  {
+    this.selectedEmployee=employee;
+  }
+  
+
+  searchByName()
+  {
+    if(this.searchTerm=="" || this.searchTerm==null)
+    {
+      Swal.fire({
+        title: 'Warning!',
+        text: "search field is empty!",
+        icon: 'error',
+        confirmButtonText: 'Ok'
+      })
+      return
+    }
+    this.employeeService.searchEmployeeByName(this.searchTerm).subscribe(response=>{
+      if(response.employees.length!=null&&response.employees.length!=0)
+        this.employeesDetail = response.employees;
+      else
+      {
+        Swal.fire({
+          title: 'Warning!',
+          text: "No search result found",
+          icon: 'error',
+          confirmButtonText: 'Ok'
+        })
+      }
+    });
+  }
+
+  downloadDocument(employee:any)
+  {
+    debugger
+    
+      if(employee.document==null || employee.document =="" || employee.document =="undefined")
+      {
+        Swal.fire({
+          title: 'Warning!',
+          text: "No Document Found",
+          icon: 'info',
+          confirmButtonText: 'Ok'
+        })
+        return 
+      }
+      let filepdf = employee.document
+      let a = document.createElement('a');
+      a.href = filepdf;
+      a.download = employee.initials;
+      a.click();
+  
   }
 
 }
