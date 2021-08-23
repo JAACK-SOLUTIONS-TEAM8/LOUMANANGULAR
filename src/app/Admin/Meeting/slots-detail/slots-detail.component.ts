a477413ba2eea92c0f806501214dcfb951ddf24a
kayyborra <u19020393@tuks.co.za>
2021-08-23 16:00:08 +0200

    added slots detail comp methods

--- src/app/Admin/Meeting/slots-detail/slots-detail.component.ts
+++ src/app/Admin/Meeting/slots-detail/slots-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MeetingService } from 'src/app/services/meeting/meeting.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-slots-detail',
@@ -7,9 +10,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SlotsDetailComponent implements OnInit {
 
-  constructor() { }
+  searchDate!:Date;
+  slotData:any[]=[];
+
+  selectedSlot:any;
+
+  constructor(
+    private router:Router,
+    private meetingService:MeetingService
+  ) { }
 
   ngOnInit(): void {
+    this.getAllSlots();
+  }
+  getAllSlots()
+  {
+      this.meetingService.getAllSlots().subscribe(response=>{
+        this.slotData=response.slots;
+      });
+  }
+
+  deleteSlot(slot:any){
+    this.selectedSlot=slot;
+  }
+
+  confirmDeleteion()
+  {
+    debugger
+      this.meetingService.deleteSlot(this.selectedSlot.slotId).subscribe(response=>{
+        
+          this.getAllSlots();
+      });
+  }
+
+  searchByDate()
+  {
+    debugger
+    if(this.searchDate!=null || this.searchDate!=undefined)
+    {
+      this.meetingService.searchSlotByDate(this.searchDate.toString()).subscribe(response=>{
+        this.slotData=response.slots;
+      })
+    }
+    else
+    {
+      Swal.fire({
+        title: 'Error!',
+        text: 'No search result found!',
+        icon: 'success',
+        confirmButtonText: 'Ok'
+      });
+    }
+  }
+
+  editSlot(slot:any)
+  {
+    this.router.navigateByUrl(`/admin/meeting/add-slot/${slot.slotId}`);
   }
 
 }
+
+
