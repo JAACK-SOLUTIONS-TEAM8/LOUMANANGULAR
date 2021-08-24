ce573da1dbcc3a37ec518d3f444932cc6933beda
JayyyHarden <u19107430@tuks.co.za>
2021-08-24 22:02:01 +0200

    Added Monthly stock component and product services

--- src/app/report-sub-system/monthly-stock-report/monthly-stock-report.component.ts
+++ src/app/report-sub-system/monthly-stock-report/monthly-stock-report.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product/product.service';
+import html2PDF from 'jspdf-html2canvas';
 
 @Component({
   selector: 'app-monthly-stock-report',
@@ -7,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MonthlyStockReportComponent implements OnInit {
 
-  constructor() { }
+  productData:any[]=[]
+  dateData:any;
 
+  constructor(
+    private productService:ProductService
+  ) {}
+  
+  
+  
   ngOnInit(): void {
   }
 
+ 
+
+  getStockMonthlyReport()
+  {
+    if(this.dateData=="" || this.dateData==null)
+    {
+      return;
+    }
+    this.productService.getStockMonthlyReport(String(this.dateData)).subscribe(response=>{
+      this.productData=response.products;
+    })
+  }
+  downloadPDF() {
+  
+
+    var element=document.getElementById("report");
+    html2PDF(element, {
+      jsPDF: {
+        format: 'a4',
+      },
+      imageType: 'image/jpeg',
+      output: `Monthly_Stock_report.pdf`
+    });
+
+}
 }
--- src/app/services/product/product.service.ts
+++ src/app/services/product/product.service.ts
@@ -1,9 +1,119 @@
 import { Injectable } from '@angular/core';
+import { DataService } from '../data/data.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  constructor(
+    private dataService:DataService
+  ) { }
+
+  getAllProducts() {
+    debugger
+    return this.dataService.genericCaller("get", "Product/All", "");
+  }
+
+  addProduct(productData: any) {
+    debugger
+    return this.dataService.genericCaller("post", "Product/Add", productData);
+  }
+
+  getProductById(productId: number) {
+    debugger
+    return this.dataService.genericCaller("get", `Product/${productId}`, "");
+  }
+
+  deleteProduct(productId: number) {
+    debugger
+    return this.dataService.genericCaller("get", `Product/Delete/${productId}`, "");
+  }
+
+  searchProductByName(searchTerm:string)
+  {
+    return this.dataService.genericCaller("get","Product/Search?name="+searchTerm,"");
+  }
+
+
+  //product types
+  getAllProductTypes()
+  {
+    return this.dataService.genericCaller("get","Product/ProductType/All","");
+
+  }
+
+  addProductType(productTypeData: any) {
+    debugger
+    return this.dataService.genericCaller("post", "Product/ProductType/Add", productTypeData);
+  }
+
+  getProductTypeById(productTypeId: number) {
+    debugger
+    return this.dataService.genericCaller("get", `Product/ProductType/${productTypeId}`, "");
+  }
+
+  deleteProductType(productTypeId: number) {
+    debugger
+    return this.dataService.genericCaller("get", `Product/ProductType/Delete/${productTypeId}`, "");
+  }
+
+  searchProductTypeByName(searchTerm:string)
+  {
+    return this.dataService.genericCaller("get","Product/ProductType/Search?name="+searchTerm,"");
+  }
+
+
+  //product types
+  getAllProductSizes()
+  {
+    return this.dataService.genericCaller("get","Product/ProductSize/All","");
+
+  }
+
+  addProductSize(productSizeData: any) {
+    debugger
+    return this.dataService.genericCaller("post", "Product/ProductSize/Add", productSizeData);
+  }
+
+  getProductSizeById(productSizeId: number) {
+    debugger
+    return this.dataService.genericCaller("get", `Product/ProductSize/${productSizeId}`, "");
+  }
+
+  deleteProductSize(productSizeId: number) {
+    debugger
+    return this.dataService.genericCaller("get", `Product/ProductSize/Delete/${productSizeId}`, "");
+  }
+
+  searchProductSizeByName(searchTerm:string)
+  {
+    return this.dataService.genericCaller("get","Product/ProductSize/Search?name="+searchTerm,"");
+  }
+
+  getStockProductById(stockId: number) {
+    debugger
+    return this.dataService.genericCaller("get", `Product/Stock/${stockId}`, "");
+  }
+
+  wireOffStockProduct(stockData: any) {
+    debugger
+    return this.dataService.genericCaller("post", `Product/Stock/WireOff`, stockData);
+  }
+
+  searchStockProductByName(searchTerm:string)
+  {
+    return this.dataService.genericCaller("get","Product/Stock/Search?name="+searchTerm,"");
+  }
+
+  checkProductQuantityInStock(productId:number)
+  {
+    return this.dataService.genericCaller("get","Product/Stock/Quantity/"+productId,"");
+  }
+
+  getStockMonthlyReport(dateInfo:string)
+  {
+    return this.dataService.genericCaller("get","Product/Stock/MonthlyReport?dateInfo="+dateInfo,"");
+  }
+
 }
