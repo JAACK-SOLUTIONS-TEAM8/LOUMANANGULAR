3b4fc9e0ca4ab69f7b8c09eb593f2f5d87c53d8b
Ammaar1803 <u18080945@tuks.co.za>
2021-08-24 16:56:06 +0200

    added login component

--- src/app/user-sub-system/login/login.component.ts
+++ src/app/user-sub-system/login/login.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AppComponent } from 'src/app/app.component';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm!:FormGroup;
+  
+  constructor(
+    private formBuilder:FormBuilder,
+    private authServcie:AuthService,
+    private router:Router
+  ) { }
 
   ngOnInit(): void {
+    this.initlizieForm();
+  }
+
+  initlizieForm()
+  {
+    this.loginForm=this.formBuilder.group({
+      userName:[null,Validators.required],
+      password:[null,Validators.required]
+    });
+  }
+
+  login()
+  {
+    if(this.loginForm.invalid)
+    {
+      Swal.fire({
+        title: 'Warning!',
+        text: "provide all fields values!",
+        icon: 'warning',
+        confirmButtonText: 'Ok'
+      })
+      return;
+    }
+    let loginData={
+      "userName":String(this.loginForm.controls["userName"].value),
+      "password":String(this.loginForm.controls["password"].value)
+    };
+
+    this.authServcie.login(loginData).subscribe(response=>{
+      if(response.statusCode==200)
+      {
+        Swal.fire({
+          title: 'Succes!',
+          text: "Success",
+          icon: 'success',
+          confirmButtonText: 'Ok'
+        }).then(()=>{
+         
+          localStorage.setItem("User",JSON.stringify(response.user));
+          this.router.navigateByUrl("/home")
+  
+        })
+      }
+      else
+      {
+        Swal.fire({
+          title: 'Warning!',
+          text: "Incorrect Username and password!",
+          icon: 'warning',
+          confirmButtonText: 'Ok'
+        })
+        return;
+      }
+    });
   }
 
 }
