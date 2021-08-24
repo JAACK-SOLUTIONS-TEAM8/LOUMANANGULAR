bbdbf26b0c5d7edf4fb58424867308104fa76d40
JayyyHarden <u19107430@tuks.co.za>
2021-08-24 22:17:01 +0200

    Added half yearly employee screen

--- src/app/report-sub-system/half-yearly-employee-report/half-yearly-employee-report.component.ts
+++ src/app/report-sub-system/half-yearly-employee-report/half-yearly-employee-report.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { EmployeeService } from 'src/app/services/employee/employee.service';
+import html2PDF from 'jspdf-html2canvas';
+declare var $:any;
 
 @Component({
   selector: 'app-half-yearly-employee-report',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HalfYearlyEmployeeReportComponent implements OnInit {
 
-  constructor() { }
+  reportData:any[]=[]
+  dateData:any;
 
+  
+  constructor(
+    private employeeService:EmployeeService
+  ) {}
+  
+  
+  
   ngOnInit(): void {
+    this.getSixMonthRegistrationReport()
   }
 
+  getSixMonthRegistrationReport()
+  {
+    
+    this.employeeService.sixMonthRegistrationHistory().subscribe(response=>{
+      this.reportData=response.history;
+    })
+  }
+
+ downloadPDF() {
+  
+
+var element=document.getElementById("report");
+html2PDF(element, {
+  jsPDF: {
+    format: 'a4',
+  },
+  imageType: 'image/jpeg',
+  output: `Half_Yearly_Employee_Report.pdf`
+});
+  
+
+}
 }
