22b55b52d2f53c21d3a0f75a016a92a9b5b72ef1
Ammaar1803 <u18080945@tuks.co.za>
2021-08-25 15:27:44 +0200

    added reset password initial step method component

--- src/app/user-sub-system/reset-password-initial-step/reset-password-initial-step.component.ts
+++ src/app/user-sub-system/reset-password-initial-step/reset-password-initial-step.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-reset-password-initial-step',
@@ -7,9 +11,80 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResetPasswordInitialStepComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private formBuilder:FormBuilder,
+    private authService:AuthService,
+    private router:Router
+  ) { }
+
+  infoForm:FormGroup;
+
 
   ngOnInit(): void {
+    this.infoForm=this.formBuilder.group({
+      userName:[null,Validators.required],
+      email:[null,Validators.required],
+      password:[null,Validators.required],
+      confirmPassword:[null,Validators.required]
+
+    })
+  }
+
+  submit()
+  {
+    if(this.infoForm.invalid)
+    {
+      Swal.fire({
+        title: 'Info!',
+        text: 'Provide all fields values',
+        icon: 'info',
+        confirmButtonText: 'Ok'
+      })
+     return
+  }
+  else if(this.infoForm.controls["password"].value!=this.infoForm.controls["confirmPassword"].value)
+  {
+    Swal.fire({
+      title: 'Info!',
+      text: 'Password does not matched!',
+      icon: 'info',
+      confirmButtonText: 'Ok'
+    })
+   return
+  }
+
+  let userInfo={
+    "userName":String(this.infoForm.controls["userName"].value),
+    "email":String(this.infoForm.controls["email"].value),
+    "password":String(this.infoForm.controls["password"].value),
+  }
+
+
+  this.authService.resetPasswordInitial(userInfo).subscribe(response=>{
+    if(response.statusCode==200)
+    {
+      Swal.fire({
+        title: 'Sucess!',
+        text: 'Password Changed Successfully!',
+        icon: 'success',
+        confirmButtonText: 'Ok'
+      }).then(()=>{
+        this.router.navigateByUrl("/login")
+
+      });
+    }
+    else
+    {
+      Swal.fire({
+        title: 'Info!',
+        text: 'User Doesnot exist',
+        icon: 'info',
+        confirmButtonText: 'Ok'
+      })
+      return
+    }
+  })
+
   }
 
 }
