94a63c4b8cff17d5ac234274a6b12772186d0f64
Ammaar1803 <u18080945@tuks.co.za>
2021-08-25 16:43:36 +0200

    is logged in component

--- src/app/user-sub-system/is-logged-in.guard.ts
+++ src/app/user-sub-system/is-logged-in.guard.ts
@@ -1,14 +1,29 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IsLoggedInGuard implements CanActivate {
+
+  constructor(
+    private router:Router
+  ) {
+
+  }
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+      var user=localStorage.getItem("User");
+      if(user==null || user == undefined)
+      {
+        this.router.navigateByUrl("/login")
+        return false;
+      }
+
     return true;
   }
   
