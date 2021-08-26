dba0f971a48ae4e56941d0b72ae0ea66e17a9597
kayyborra <u19020393@tuks.co.za>
2021-08-26 14:47:43 +0200

    added employee profile methods

--- src/app/employee-sub-system/employee-profile/employee-profile.component.ts
+++ src/app/employee-sub-system/employee-profile/employee-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { EmployeeService } from 'src/app/services/employee/employee.service';
 
 @Component({
   selector: 'app-employee-profile',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeProfileComponent implements OnInit {
 
-  constructor() { }
+  employeeId:number;
+  teamId:number;
+  employeeData:any={}
+
+  constructor(
+    private employeeService:EmployeeService,
+    private route:ActivatedRoute,
+    private router:Router
+  ) {
+    this.route.params.subscribe(params=>{
+      this.teamId=params["teamId"]
+      this.employeeId=params["employeeId"]
+
+    })
+   }
 
   ngOnInit(): void {
+    this.employeeService.getEmployeeById(this.employeeId).subscribe(response=>{
+      this.employeeData=response.employee
+    })
   }
 
+  goBack(){
+    this.router.navigateByUrl("/admin/team/manage/"+this.teamId)
+}
+
+downloadDocument(employee:any)
+{  
+    let filepdf = employee.document
+    let a = document.createElement('a');
+    a.href = filepdf;
+    a.download = employee.initials;
+    a.click();
+
+}
 }
