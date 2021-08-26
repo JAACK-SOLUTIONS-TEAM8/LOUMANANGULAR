0f7375a01543e2197c3ad70ea470a1bb5c9a65fb
JayyyHarden <u19107430@tuks.co.za>
2021-08-26 16:05:30 +0200

    removed duplicate import

--- src/app/order-sub-system/shopping-cart/shopping-cart/shopping-cart.component.ts
+++ src/app/order-sub-system/shopping-cart/shopping-cart/shopping-cart.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
-import { Component, OnInit } from '@angular/core';
 import { CartService } from 'src/app/services/cart/cart.service';
 import Swal from 'sweetalert2';
 
