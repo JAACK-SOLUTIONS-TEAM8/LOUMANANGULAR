43e3c511ea3ee71155ec2cfe41543aa8b7a6e318
Ammaar1803 <u18080945@tuks.co.za>
2021-10-01 01:47:41 +0200

    auth service ts fixed

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { DataService } from '../data/data.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  isLoggedIn:boolean=false;
+  user:string=null;
+
+  isLoggedInSubject:BehaviorSubject<boolean>=new BehaviorSubject<boolean>(this.isLoggedIn);
+  isLoggedIn$:Observable<boolean>=this.isLoggedInSubject.asObservable();
+
+  userSubject:BehaviorSubject<string>=new BehaviorSubject<string>(this.user);
+  user$:Observable<string>=this.userSubject.asObservable();
 
   constructor(
     private dataService:DataService
