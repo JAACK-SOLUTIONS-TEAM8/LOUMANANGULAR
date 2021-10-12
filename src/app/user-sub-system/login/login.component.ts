9cafb822c2b9eef071c78c537fdb44a5518d348a
Ammaar1803 <u18080945@tuks.co.za>
2021-10-12 16:39:28 +0200

    login ts file update

--- src/app/user-sub-system/login/login.component.ts
+++ src/app/user-sub-system/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth/auth.service';
+import { TimerConfigService } from 'src/app/services/timer/timer-config.service';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -12,11 +13,16 @@ import Swal from 'sweetalert2';
 export class LoginComponent implements OnInit {
 
   loginForm:FormGroup;
-  
+  code:string="";
+
+  isAuthenticated:boolean=false;
+
   constructor(
     private formBuilder:FormBuilder,
     private authServcie:AuthService,
-    private router:Router
+    private router:Router,
+    private timerService:TimerConfigService,
+    private authService:AuthService
   ) { }
 
   ngOnInit(): void {
@@ -27,7 +33,8 @@ export class LoginComponent implements OnInit {
   {
     this.loginForm=this.formBuilder.group({
       userName:[null,Validators.required],
-      password:[null,Validators.required] 
+      password:[null,Validators.required],
+      code:[null] 
     });
   }
 
@@ -51,24 +58,17 @@ export class LoginComponent implements OnInit {
     this.authServcie.login(loginData).subscribe(response=>{
       if(response.statusCode==200)
       {
-        localStorage.setItem('isLoggedIn',String(true))
+        this.isAuthenticated=true;
 
-        this.authServcie.isLoggedIn=true;
-        this.authServcie.user=JSON.stringify(response.user);
+        localStorage.setItem('userToConfirm',JSON.stringify(response.user));
 
-        this.authServcie.isLoggedInSubject.next(true);
-        this.authServcie.userSubject.next(JSON.stringify(response.user))
         Swal.fire({
           title: 'Succes!',
           text: "Success",
           icon: 'success',
           confirmButtonText: 'Ok'
-        }).then(()=>{
-         
-          localStorage.setItem("User",JSON.stringify(response.user));
-          this.router.navigateByUrl("/home")
-  
         })
+        return
       }
       else
       {
@@ -85,5 +85,79 @@ export class LoginComponent implements OnInit {
       }
     });
   }
+
+
+  verifyCode(){
+    debugger
+    let verificationCode=this.loginForm.controls["code"].value
+    if(verificationCode==null ||verificationCode=="")
+    {
+      
+      Swal.fire({
+        title: 'Warning!',
+        text: "Please Enter Verification Code",
+        icon: 'warning',
+        confirmButtonText: 'Ok'
+      })
+      return;
+    }
+    this.authService.verify({
+      user:JSON.parse(localStorage.getItem("userToConfirm")),
+      code:verificationCode
+    }).subscribe(response=>{
+
+
+      if(response.statusCode==200)
+      {
+
+        console.log(response.status)
+        localStorage.setItem('isLoggedIn',String(true))
+
+        this.authServcie.isLoggedIn=true;
+        this.authServcie.user=JSON.stringify(response.status.user);
+
+        this.authServcie.isLoggedInSubject.next(true);
+        this.authServcie.userSubject.next(JSON.stringify(response.status.user))
+
+        localStorage.setItem("User",JSON.stringify(response.status.user));
+
+        this.timerService.getTimerConfig().subscribe(response=>{
+
+        localStorage.setItem("timerConfig",JSON.stringify(response.config));
+        })
+        Swal.fire({
+          title: 'Success!',
+          text: "User Verified",
+          icon: 'success',
+          confirmButtonText: 'Ok'
+        }).then(()=>{
+         
+          this.router.navigateByUrl("/home")
+  
+        })
+      }
+      else if(response.statusCode == 401)
+      {
+        Swal.fire({
+          title: 'Warning!',
+          text: "Incorrect Code",
+          icon: 'warning',
+          confirmButtonText: 'Ok'
+        })
+        return
+      }
+      else
+      {
+        Swal.fire({
+          title: 'Warning!',
+          text: "Incorrect code or time out try again",
+          icon: 'warning',
+          confirmButtonText: 'Ok'
+        })
+      }
+      console.log(response);
+    })
+  }
+
 
 }
